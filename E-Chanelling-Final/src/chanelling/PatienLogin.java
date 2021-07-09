9e258bdb6340eedb153a202ddb96adf3514ac662
RashmiICT <96565@fhss.sjp.ac.lk>
2021-07-10 00:13:48 +0530

    change button

--- E-Chanelling-Final/src/chanelling/PatienLogin.java
+++ E-Chanelling-Final/src/chanelling/PatienLogin.java
@@ -212,7 +212,7 @@ public class PatienLogin extends javax.swing.JFrame {
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
-        DoctorDetails dd = new DoctorDetails();
+        Viewdoc dd = new Viewdoc();
         dd.setVisible(true);
         
         
