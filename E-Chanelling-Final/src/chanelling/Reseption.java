cd8247eec14d5a35783d1cff062d4711017f8197
RashmiICT <96565@fhss.sjp.ac.lk>
2021-07-09 11:23:08 +0530

    link button

--- E-Chanelling-Final/src/chanelling/Reseption.java
+++ E-Chanelling-Final/src/chanelling/Reseption.java
@@ -228,6 +228,9 @@ public class Reseption extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        Patientdetailsveiw pd = new Patientdetailsveiw();
+        pd.setVisible(true);
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
