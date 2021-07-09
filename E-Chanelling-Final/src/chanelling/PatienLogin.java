1334ec01c20e6438669699ed935cb4063a84b983
RashmiICT <96565@fhss.sjp.ac.lk>
2021-07-09 11:15:13 +0530

    connect button

--- E-Chanelling-Final/src/chanelling/PatienLogin.java
+++ E-Chanelling-Final/src/chanelling/PatienLogin.java
@@ -212,6 +212,8 @@ public class PatienLogin extends javax.swing.JFrame {
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
+        DoctorDetails dd = new DoctorDetails();
+        dd.setVisible(true);
         
         
     }//GEN-LAST:event_jButton6ActionPerformed
