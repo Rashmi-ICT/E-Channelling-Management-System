cc30c73e85bfc7b7d3dfd5a45581096a66c7d696
RashmiICT <96565@fhss.sjp.ac.lk>
2021-07-10 22:09:01 +0530

    link button

--- E-Chanelling-Final/src/chanelling/homePage.java
+++ E-Chanelling-Final/src/chanelling/homePage.java
@@ -137,6 +137,7 @@ public class homePage extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         
+        
         Password pas  = new Password();
         pas.setVisible(true); 
     }//GEN-LAST:event_jButton2ActionPerformed
