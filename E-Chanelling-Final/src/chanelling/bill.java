b70e052bb2f41b7993a44b6fbd04445f60f2ab8e
RashmiICT <96565@fhss.sjp.ac.lk>
2021-07-14 14:02:02 +0530

    change button

--- E-Chanelling-Final/src/chanelling/bill.java
+++ E-Chanelling-Final/src/chanelling/bill.java
@@ -216,7 +216,7 @@ public class bill extends javax.swing.JFrame {
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("PATIENT ID");
+        jLabel3.setText("CHANEL ID");
 
         cmbPatientId.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
